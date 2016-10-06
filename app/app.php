e4f4b89832e08b99576f6d43516aa5d590326fb9
Tim <tim.bourgault@gmail.com>
2016-10-06 14:10:11 -0700

    set up app.php and index.html.twig files to start building front end.

---
+++ app/app.php
@@ -0,0 +1,30 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
+    date_default_timezone_set('America/Los_Angeles');
+
+    use Symfony\Component\Debug\Debug;
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    Debug::enable();
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    // //ALTERNATIVE SERVER:
+    $server = 'mysql:host=localhost;dbname=shoe_stores';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+return $app;
+
+?>

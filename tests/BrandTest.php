bcc68df9b84bf8b65d1c1e3db9bea11044ab1cd4
Tim <tim.bourgault@gmail.com>
2016-10-06 10:56:57 -0700

    create test1 in BrandTest file which passes because getName function was already created to make addBrand test pass in StoreTest file.

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -5,22 +5,33 @@
     * @backupsStaticAttributes disabled
     **/
 
-    // require_once "src/Brand.php";
-    //
-    // $server = 'mysql:host=localhost;dbname=shoe_stores_test';
-    // $username = 'root';
-    // $password = 'root';
-    // $DB = new PDO($server, $username, $password);
-    //
-    // class BrandTest extends PHPUnit_Framework_TestCase
-    // {
-    //
-    //     protected function tearDown()
-    //     {
-    //         Brand::deleteAll();
-    //         Store::deleteAll();
-    //     }
-    //
-    //
-    //  }
+    require_once "src/Brand.php";
+    require_once "src/Store.php";
+
+    $server = 'mysql:host=localhost;dbname=shoe_stores_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class BrandTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+            Brand::deleteAll();
+            Store::deleteAll();
+        }
+
+        function test_getName()
+        {
+            // Arrange
+            $test_brand = new Brand("Vibram");
+            // Act
+            $output = $test_brand->getName();
+            // Assert
+            $this->assertEquals("Vibram", $output);
+        }
+
+
+     }
 ?>
